37c9e78cc4f7f124986c61bcee23624d67ed3847
lahcenassmira <lahcenassmira@gmail.com>
2024-05-01 13:00:53 -0700

    stock latitude and longitude data into a database

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express');
+const Datastore = require('nedb');
 const app = express();
-const fs = require('fs');
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' })); //limit to 1mb for data comes from body
+   
+
+  const database = new Datastore('database.db');
+   database.loadDatabase();
 
 app.post('/api', (req, res) => {
     console.log("I got a request");
-    console.log(req.body);
     const data = req.body;
-    const db = [];
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+    database.insert(data);
     res.json({
         status: 'Success',
+        timestamp: timestamp,
         latitude: data.lat,
         longitude: data.lon
     });
